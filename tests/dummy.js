da28a0c06a355c11ccfae1d7d3d859a8e504445d
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2022-10-15 01:20:58 +0200

    Update test for markdown parsing

--- tests/dummy.js
+++ tests/dummy.js
@@ -1,6 +1,9 @@
-import { mount } from '@vue/test-utils'
+import { Markdown } from '@nuxt/content/parsers'
+import { getOptions } from '@nuxt/content'
+
+const markdown = new Markdown(getOptions().markdown)
 
 describe('Dummy', () => {
-  test('is a test', () => {
+  test('is a test', async () => {
   })
 })
