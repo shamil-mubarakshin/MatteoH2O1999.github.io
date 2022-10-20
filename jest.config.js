4824a47dcaba5ecb61ebc847dd1a73a0c918f92b
Matteo Dell'Acqua <82184604+MatteoH2O1999@users.noreply.github.com>
2022-10-20 14:02:32 +0200

    Disable coverage folder creation

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
     '.*\\.(vue)$': 'vue-jest'
   },
   collectCoverage: true,
+  coverageReporters: ['text'],
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue',
