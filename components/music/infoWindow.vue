877ba6d28c45e2e1856141e3e80a0969018547d5
Matteo Dell'Acqua <82184604+MatteoH2O1999@users.noreply.github.com>
2022-11-04 14:16:13 +0100

    Add computed translated title and description for music cards

--- components/music/infoWindow.vue
+++ components/music/infoWindow.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="additional-info-tab">
-        Test infoWindow {{ data }}
+        <h1>{{ title }}</h1>
+        {{ description }}
         <button @click="closeTab()">Test close</button>
     </div>
 </template>
@@ -16,6 +17,14 @@
         methods: {
             closeTab() {
                 this.$emit('closeTab');
+            }
+        },
+        computed: {
+            title() {
+                return this.data.lang[this.$i18n.locale].title;
+            },
+            description() {
+                return this.data.lang[this.$i18n.locale].description;
             }
         }
     }
