d82eb0a1e5493f3150a0f9ab9e97e41f0f7c7923
Matteo Dell'Acqua <82184604+MatteoH2O1999@users.noreply.github.com>
2023-01-13 14:27:02 +0100

    Fix about testing

---
+++ tests/about.content.test.js
@@ -0,0 +1,43 @@
+import { Markdown } from '@nuxt/content/parsers';
+import { getOptions } from '@nuxt/content';
+import { describe, test, expect } from '@jest/globals';
+import fs from 'fs';
+import defaults from '../nuxt.config'
+
+const locales = getLocales();
+
+const markdown = new Markdown(getOptions().markdown);
+
+function getLocales() {
+    let localeObject = defaults.i18n.locales;
+    let localeList = [];
+    localeObject.forEach(element => {
+        localeList.push(element.code);
+    })
+    return localeList;
+}
+
+describe('About page', () => {
+    const folderPath = './content/about';
+
+    test.each(locales)('is written for locale %s', (locale) => {
+        const aboutPage = folderPath + '/' + locale + '.md';
+
+        expect(fs.existsSync(aboutPage)).toEqual(true);
+    })
+
+    test('each locale has similar length', async () => {
+        const lengths = [];
+        for (const locale of locales) {
+            const path = folderPath + '/' + locale + '.md';
+            const file = fs.readFileSync(path, {encoding: 'utf-8'});
+            const parsed = await markdown.toJSON(file);
+            lengths.push(parsed.text.length);
+        }
+        for (const l1 of lengths) {
+            for (const l2 of lengths) {
+                expect(Math.abs(Math.log10(l1) - Math.log10(l2))).toBeLessThan(0.25);
+            }
+        }
+    })
+})
