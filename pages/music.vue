7eefbe3b902c31c9fe950efd5d182bf69e82bb08
Matteo Dell'Acqua <82184604+MatteoH2O1999@users.noreply.github.com>
2023-03-06 15:06:47 +0100

    Comment out unused sections

--- pages/music.vue
+++ pages/music.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="music-credits">
         <MusicCreditTab subFolder="movies" :tabTitle="$t('musicCreditsTabs.movies')" />
-        <MusicCreditTab subFolder="musicals" :tabTitle="$t('musicCreditsTabs.musicals')" />
-        <MusicCreditTab subFolder="games" :tabTitle="$t('musicCreditsTabs.games')" />
-        <MusicCreditTab subFolder="albums" :tabTitle="$t('musicCreditsTabs.albums')" />
-        <MusicCreditTab subFolder="singles" :tabTitle="$t('musicCreditsTabs.singles')" />
-        <MusicCreditTab subFolder="unfinished" :tabTitle="$t('musicCreditsTabs.unfinished')" />
+        <!--MusicCreditTab subFolder="musicals" :tabTitle="$t('musicCreditsTabs.musicals')" /-->
+        <!-- <MusicCreditTab subFolder="games" :tabTitle="$t('musicCreditsTabs.games')" /> -->
+        <!-- <MusicCreditTab subFolder="albums" :tabTitle="$t('musicCreditsTabs.albums')" /> -->
+        <!-- <MusicCreditTab subFolder="singles" :tabTitle="$t('musicCreditsTabs.singles')" /> -->
+        <!-- <MusicCreditTab subFolder="unfinished" :tabTitle="$t('musicCreditsTabs.unfinished')" /> -->
     </div>
 </template>
 
