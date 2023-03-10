1e4b6173849874550090e67b611b10bd5cf9ad68
Matteo Dell'Acqua <82184604+MatteoH2O1999@users.noreply.github.com>
2023-03-10 14:53:41 +0100

    Add page selector skeleton

---
+++ components/blog/pageSelector.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="page-selector-wrapper">
+        Page Selector {{ this.currentPage }} {{this.lastPage}}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'pageSelector'
+        }
+    },
+    props: {
+        currentPage: Number,
+        lastPage: Number
+    }
+}
+</script>
+
+<style>
+</style>
