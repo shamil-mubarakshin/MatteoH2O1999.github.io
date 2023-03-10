e3067f51cbc1c456ef9d5846530615283cd6d50a
Matteo Dell'Acqua <82184604+MatteoH2O1999@users.noreply.github.com>
2023-03-10 14:55:13 +0100

    Add blog list to projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,8 +1,12 @@
 <template>
-    <h1>Projects</h1>
+    <div>
+        <BlogArticleList :articles="this.articles" />
+        <BlogPageSelector :currentPage="this.currentPage" :lastPage="this.lastPage"/>
+    </div>
 </template>
 
 <script>
+const articlesPerPage = 10;
 export default {
     data() {
         return {
@@ -13,6 +17,23 @@ export default {
         return {
             title: "[WIP] Matteo Dell'Acqua | Projects"
         }
+    },
+    async asyncData(context) {
+        const urlParams = new URLSearchParams(context.route.query);
+        const pageNumber = urlParams.get('page');
+        let page = 0
+        if (pageNumber !== null) {
+            page = parseInt(pageNumber);
+        }
+        const articles = [];
+        const content = await context.$content('articles', context.i18n.locale, {deep: true}).sortBy('createdAt', 'desc').skip(articlesPerPage * page).fetch();
+        articles.push(...content.slice(0, articlesPerPage));
+        if (articles.length === 0) {
+            context.error({statusCode: 404, message: 'Page not found'});
+        }
+        const currentPage = page + 1;
+        const lastPage = Math.ceil((content.length - articlesPerPage) / articlesPerPage) + page + 1;
+        return {articles, currentPage, lastPage};
     }
 }
 </script>
