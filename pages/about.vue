11aac241f59450dbdf735f46d3e923219447eafc
Matteo Dell'Acqua <82184604+MatteoH2O1999@users.noreply.github.com>
2023-04-21 13:55:37 +0200

    Improve about page layout

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,11 @@
 <template>
     <article class="about-article">
-        <nuxt-content :document="aboutParagraph" />
+        <div class="about-section">
+            <nuxt-img format="webp" src="/homePage/homePagePhoto.png" class="about-image" />
+            <div class="about-paragraph">
+                <nuxt-content :document="aboutParagraph" />
+            </div>
+        </div>
     </article>
 </template>
 
@@ -28,8 +33,40 @@ export default {
 
 <style>
 .about-article {
-    width: 90vw;
+    width: 70vw;
     margin-left: auto;
     margin-right: auto;
+}
+
+.about-section {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: flex-start;
+}
+
+.about-paragraph {
+    flex-grow: 1;
+}
+
+.about-image {
+    width: 30vw;
+    aspect-ratio: auto 2 / 3;
+    flex-grow: 0;
+}
+
+@media screen and (max-width: 600px) {
+    .about-article {
+        width: 90vw;
+    }
+
+    .about-section {
+        flex-direction: column;
+    }
+
+    .about-image {
+        width: 100%;
+        flex-grow: initial;
+    }
 }
 </style>
