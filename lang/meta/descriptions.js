a805eafc347b48460d95bcd1d72dcf52623684bf
Matteo Dell'Acqua <82184604+MatteoH2O1999@users.noreply.github.com>
2023-05-04 15:00:15 +0200

    Add final meta descriptions to static pages

--- lang/meta/descriptions.js
+++ lang/meta/descriptions.js
@@ -1,20 +1,20 @@
 // TODO
 let descriptions_en_us = {
-    projects: "Projects",
-    about: "About",
-    games: "Games",
-    index: "Homepage",
-    music: "Music",
-    news: "News"
+    projects: "Discover news about Matteo Dell'Acqua's projects.",
+    about: "Biography and information about Matteo Dell'Acqua.",
+    games: "Discover videogames created by Matteo Dell'Acqua.",
+    index: "Matteo Dell'Acqua's homepage.",
+    music: "Explore and listen to music composed or arranged by Matteo Dell'Acqua.",
+    news: "Read the latest news from Matteo Dell'Acqua."
 }
 
 let descriptions_it_it = {
-    projects: "Progetti",
-    about: "Chi sono",
-    games: "Videogiochi",
-    index: "Pagina principale",
-    music: "Musica",
-    news: "Novità"
+    projects: "Scopri le novità sui progetti di Matteo Dell'Acqua.",
+    about: "Biografia ed informazioni su Matteo Dell'Acqua.",
+    games: "Scopri i videogiochi realizzati da Matteo Dell'Acqua",
+    index: "Pagina principale di Matteo Dell'Acqua.",
+    music: "Esplora ed ascolta la musica composta o arrangiata da Matteo Dell'Acqua.",
+    news: "Leggi le ultime novità da Matteo Dell'Acqua."
 }
 
 export {
